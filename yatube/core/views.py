e4867f049e3db46cd41bf352a9b565fcfe1103cd
Aud1cious <justanotherref@gmail.com>
2022-12-23 20:06:54 +0400

    pep8

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -1,11 +1,14 @@
 # core/views.py
 from django.shortcuts import render
 
+
 def page_not_found(request, exception):
     return render(request, 'core/404.html', {'path': request.path}, status=404)
 
+
 def csrf_not_found(request, reason=''):
     return render(request, 'core/403csrf.html')
+
 
 def server_error(request):
     return render(request, 'core/500_server_error.html', status=500)
--- yatube/posts/tests/test_image.py
+++ yatube/posts/tests/test_image.py
@@ -16,7 +16,11 @@ class PostFormTests(TestCase):
     def setUpClass(cls):
         super().setUpClass()
 
-        cls.test_gif = b"\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\x80\x00\x00\x05\x04\x04\x00\x00\x00\x2c\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02\x44\x01\x00\x3b"
+        cls.test_gif = (
+            b"\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\x80\x00"
+            b"\x00\x05\x04\x04\x00\x00\x00\x2c\x00\x00\x00\x00"
+            b"x01\x00\x01\x00\x00\x02\x02\x44\x01\x00\x3b"
+        )
         cls.create_gif = SimpleUploadedFile(
             name="create.gif",
             content=cls.test_gif,
@@ -87,7 +91,7 @@ class PostFormTests(TestCase):
             try:
                 posts = list(response.context.get("page_obj").object_list)
                 ind = posts.index(self.post_with_image)
-            except Exception as e:
+            except Exception:
                 self.assertFalse(False)
 
             with self.subTest(url=url):
